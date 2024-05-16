1f41de93f5b21d63287cc8a18d727b1110cf56e7
SebSpr <sebastian.spreitzer@uibk.ac.at>
2024-05-16 15:53:37 +0200

    labels added

--- main.js
+++ main.js
@@ -35,8 +35,21 @@ async function showForecast(url) {
     let response = await fetch(url);
     let jsondata = await response.json();
     L.geoJSON(jsondata, {
-        pointToLAyer: function(feature, latlng){
-            
+        pointToLayer: function (feature, latlng) {
+            var content = `
+            <ul>
+                <li>Luftdruck (hPa): air_pressure_at_sea_level</li>
+                <li>Lufttemperatur (°C): air_temperature</li>
+                <li>Bewölkungsgrad (%): cloud_area_fraction</li>
+                <li>Niederschlagsmenge (mm): percipitation_amount</li>
+                <li>Relative Feuchte (%): relative_humidity</li>
+                <li>Windrichtung (°): wind_from_direction</li>
+                <li>Windgeschwindigkeit (m/s): wind_speed</li>
+            </ul>
+            `;
+            L.popup(latlng, { 
+                content: content
+         }).openOn(themaLayer.forecast);
         }
     }).addTo(themaLayer.forecast);
 
